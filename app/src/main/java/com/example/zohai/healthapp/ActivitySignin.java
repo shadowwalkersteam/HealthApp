a5e890a7b2b46171b281173c5c2246030abab141
zohaib <zohaibjamshaid99@gmail.com>
2017-03-19 22:37:45 +0500

    Splash Screen

--- app/src/main/java/com/example/zohai/healthapp/ActivitySignin.java
+++ app/src/main/java/com/example/zohai/healthapp/ActivitySignin.java
@@ -5,17 +5,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ActivitySignin extends AppCompatActivity {
 
 
-     ImageView signinback;
+    ImageView signinback;
+    TextView forgot;
+    TextView sign;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin);
 
         signinback = (ImageView)findViewById(R.id.signinback);
+        sign = (TextView)findViewById(R.id.signin);
+        forgot = (TextView)findViewById(R.id.forgt);
 
 
         signinback.setOnClickListener(new View.OnClickListener() {
