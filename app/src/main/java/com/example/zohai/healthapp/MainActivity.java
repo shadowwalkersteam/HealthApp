8c896b0aadcef0b3a28e431b3445ec0d2c2c37c1
zohaib <zohaibjamshaid99@gmail.com>
2017-03-19 13:37:09 +0500

    Layout Design

--- app/src/main/java/com/example/zohai/healthapp/MainActivity.java
+++ app/src/main/java/com/example/zohai/healthapp/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.zohai.healthapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView signin;
+    LinearLayout get;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        get = (LinearLayout)findViewById(R.id.get);
+        signin = (TextView)findViewById(R.id.signin);
+
+        get.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent it = new Intent(MainActivity.this,ActivitySignup.class);
+                startActivity(it);
+
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent it = new Intent(MainActivity.this,ActivitySignin.class);
+                startActivity(it);
+            }
+        });
+
     }
 }
