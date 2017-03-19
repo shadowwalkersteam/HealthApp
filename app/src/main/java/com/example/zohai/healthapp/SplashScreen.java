10de3682b207905e12977b30b2adbc9227f76e3c
zohaib <zohaibjamshaid99@gmail.com>
2017-03-19 22:28:28 +0500

    Splash Screen

--- app/src/main/java/com/example/zohai/healthapp/SplashScreen.java
+++ app/src/main/java/com/example/zohai/healthapp/SplashScreen.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
