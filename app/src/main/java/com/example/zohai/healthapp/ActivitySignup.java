0ae491d70092141f07afe7aa6baf87e3ed308000
zohaib <zohaibjamshaid99@gmail.com>
2017-07-13 22:58:31 +0500

    Signin, Signup, Reset Password, Firebase Integration

--- app/src/main/java/com/example/zohai/healthapp/ActivitySignup.java
+++ app/src/main/java/com/example/zohai/healthapp/ActivitySignup.java
@@ -90,7 +90,7 @@ public class ActivitySignup extends AppCompatActivity {
                 // the auth state listener will be notified and logic to handle the
                 // signed in user can be handled in the listener.
                 if (!task.isSuccessful()) {
-                    Log.d(TAG,"Authentication failed." + task.getException());
+                    Toast.makeText(getApplicationContext(), "Authentication failed", Toast.LENGTH_SHORT).show();
 
                 } else {
                     startActivity(new Intent(ActivitySignup.this, DatasourceID.class));
